6caeef84b869382a36822cdc06086ed523ad2aae
Roch de Vidts <roch-dv@hotmail.com>
2025-02-13 15:48:28 +0100

    New website coming

--- js/ChangeInfos.js
+++ js/ChangeInfos.js
@@ -1,79 +1,77 @@
-// alpha.lenght = 10 || span 0 à 9
-const alpha = ["S", "E", "T", " ", "D", "E", "S", "I", "G", "N"];
-// beta.lenght = 18 || span 10 à 27
-const beta = ["C", "R", "E", "A", "T", "I", "V", "E", " ", "D", "I", "R", "E", "C", "T", "I", "O", "N"];
-// charlie.lenght = 12 || span 28 à 39
-const charlie = ["A", "R", "C", "H", "I", "T", "E", "C", "T", "U", "R", "E"];
+// alpha.lenght = 10 || span 0 à 11
+const alpha = ["A", "R", "C", "H", "I", "T", "E", "C", "T", "U", "R", "E"]; // ARCHITECTURE (12 lettres)
+// beta.lenght = 18 || span 12 à 22
+const beta = ["S", "C", "E", "N", "O", "G", "R", "A", "P", "H", "Y"]; // SCENOGRAPHY (11 lettres)
+// charlie.lenght = 12 || span 23 à 35
+const charlie = ["A", "R", "T", " ", "D", "I", "R", "E", "C", "T", "I", "O", "N"]; // ART DIRECTION (13 lettres)
 
-const delta = ["E", "P", "H", "E", "M", "E", "R", "A", "L", " "];
-const echo = ["T", "I", "M", "E", "L", "E", "S", "S", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "];
-const foxtrot = ["S", "T", "R", "U", "C", "T", "U", "R", "E", " ", " ", " "];
+const delta = ["E", "P", "H", "E", "M", "E", "R", "A", "L", "", "", ""];
+const echo = ["T", "I", "M", "E", "L", "E", "S", "S", "", "", ""];
+const foxtrot = ["S", "T", "R", "U", "C", "T", "U", "R", "E", "", "", "", ""];
 
-function GetAllInfosSpan(){
-  return document.querySelectorAll('.infos span');
+function GetAllInfosSpan() {
+  return document.querySelectorAll(".infos span");
 }
 
 let infosLetters = GetAllInfosSpan();
 
-
-function DeleteLetters(){
+function DeleteLetters() {
   // toutes les 25 ms lettre change
   let x = 0;
-  let deleteLettersInterval = setInterval(function(){
-    if (x < 40){
+  let deleteLettersInterval = setInterval(function () {
+    if (x < 36) {
       infosLetters[x].style.opacity = 0;
       x++;
     } else {
       clearInterval(deleteLettersInterval);
     }
-  }, 25)
+  }, 25);
 }
 
-function WriteNewWord(){
-  if (infosLetters[0].innerHTML === "S"){
+function WriteNewWord() {
+  if (infosLetters[0].innerHTML === "A") {
     let x = 0;
-    let addNewLettersInterval = setInterval(function(){
-      if (x <= 9){
+    let addNewLettersInterval = setInterval(function () {
+      if (x <= 11) {
         infosLetters[x].innerHTML = delta[x];
         infosLetters[x].style.opacity = 1;
         x++;
-      } else if  (x >= 10 && x <= 27){
-        infosLetters[x].innerHTML = echo[x - 10];
+      } else if (x >= 12 && x <= 22) {
+        infosLetters[x].innerHTML = echo[x - 12];
         infosLetters[x].style.opacity = 1;
         x++;
-      } else if (x >= 28 && x <= 39){
-        infosLetters[x].innerHTML = foxtrot[x - 28];
+      } else if (x >= 23 && x <= 35) {
+        infosLetters[x].innerHTML = foxtrot[x - 23];
         infosLetters[x].style.opacity = 1;
         x++;
       } else {
         clearInterval(addNewLettersInterval);
       }
-    }, 25)
+    }, 25);
   } else {
     let x = 0;
-    let addNewLettersInterval = setInterval(function(){
-      if (x <= 9){
+    let addNewLettersInterval = setInterval(function () {
+      if (x <= 11) {
         infosLetters[x].innerHTML = alpha[x];
         infosLetters[x].style.opacity = 1;
         x++;
-      } else if  (x >= 10 && x <= 27){
-        infosLetters[x].innerHTML = beta[x - 10];
+      } else if (x >= 12 && x <= 22) {
+        infosLetters[x].innerHTML = beta[x - 12];
         infosLetters[x].style.opacity = 1;
         x++;
-      } else if (x >= 28 && x <= 39){
-        infosLetters[x].innerHTML = charlie[x - 28];
+      } else if (x >= 23 && x <= 35) {
+        infosLetters[x].innerHTML = charlie[x - 23];
         infosLetters[x].style.opacity = 1;
         x++;
       } else {
         clearInterval(addNewLettersInterval);
       }
-    }, 25)
+    }, 25);
   }
 }
 
-
-async function ChangeInfos(){
+async function ChangeInfos() {
   DeleteLetters();
   await sleep(1000);
-  WriteNewWord()
+  WriteNewWord();
 }
